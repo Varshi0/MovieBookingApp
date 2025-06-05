b29e759354f428c3060104c8ede0118078b920d6
Niranjan.Sunduri <niranjansunduri@gmail.com>
2025-06-05 21:23:33 +0530

    authentication

---
+++ src/main/java/com/niranjan/movie/config/KeycloakRoleConverter.java
@@ -0,0 +1,25 @@
+package com.niranjan.movie.config;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        List<String> roles = jwt.getClaimAsMap("realm_access") != null
+            ? (List<String>) ((Map<String, Object>) jwt.getClaim("realm_access")).get("roles")
+            : List.of();
+
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role)) // Spring expects "ROLE_"
+                .collect(Collectors.toList());
+    }
+}
+
